1a5b702df2fc74294c491ed1fda056ac55ea6bcd
PlusCloud <filipe.ramalho@outlook.de>
2016-12-19 16:15:22 +0100

    Added nav.php include

--- get.php
+++ get.php
@@ -1,3 +1,4 @@
+<?php
 <html>
    <head>
       <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet">
@@ -11,27 +12,11 @@
    </head>
    <body>
    
-      <nav class="navbar navbar-default navbar-static-top">
-         <div class="container">
-            <div class="navbar-header">
-               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-               </button>
-               <a class="navbar-brand" href="/">ManageMyServer</a>
-            </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-               <ul class="nav navbar-nav">
-                  <li><a href="/">Home</a></li>
-                  <li><a href="/get.html">Get MMS</a></li>
-               </ul>
-            </div><!-- /.navbar-collapse -->
-         </div><!-- /.container-fluid -->
-      </nav>
+      
+      <!-- Start including Navigation -->
+      include 'nav.php';
+      <!-- Navigation inclusion end -->
+      
       <div class="container">
          <h2 class="text-center">Get ManageMyServer</h2>
          <h3>How do I get ManageMyServer?</h3>
@@ -45,3 +30,4 @@
 <footer class="container"><hr>&copy; ManageMyServer<span class="pull-right"><a href="https://github.com/ManageMyServer">This site was made by ManageMyServer.</a></span></footer>
    </body>
 </html>
+?>
